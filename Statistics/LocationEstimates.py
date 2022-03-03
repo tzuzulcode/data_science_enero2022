9c2e969331ad4c7dd3456e8972f52fdac692483c
Tzuzul Code <mail@tzuzulcode.com>
2022-03-02 18:09:26 -0600

    Weighted mean

--- Statistics/LocationEstimates.py
+++ Statistics/LocationEstimates.py
@@ -25,3 +25,12 @@ weighted_median = wquantiles.median(state["Murder rate"],weights=state["Populati
 
 print(weighted_median)
 
+test_data = pd.Series([1,2,3,4,5])
+test_weights = pd.Series([0.15,0.1,0.2,0.3,0.25])
+
+print(test_data)
+
+weighted_median_example = wquantiles.median(test_data,weights=test_weights)
+
+print(weighted_median_example)
+
